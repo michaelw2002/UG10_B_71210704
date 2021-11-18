75e6ba226748858c14c33cf2ce0c6db648cad9ef
michael <71210704@students.ukdw.ac.id>
2021-11-18 19:53:54 +0700

    Soal Pertama UG10

---
+++ 1_B_71210704.py
@@ -0,0 +1,23 @@
+#input nilai
+n = str(input("Masukkan nilai huruf PrakTekKom anda : "))
+#kondisi dan hasil output
+if n=='A':
+ print ("Rentang nilai PrakTekKom anda adalah 85 - 100")
+elif n=='A-':
+ print ("Rentang nilai PrakTekKom anda adalah 80 - 84")
+elif n=='B+':
+ print ("Rentang nilai PrakTekKom anda adalah 75 - 79")
+elif n=='B':
+ print ("Rentang nilai PrakTekKom anda adalah 70 - 74")
+elif n=='B-':
+ print ("Rentang nilai PrakTekKom anda adalah 65 - 69")
+elif n=='C+':
+ print ("Rentang nilai PrakTekKom anda adalah 60 - 64")
+elif n=='C':
+ print ("Rentang nilai PrakTekKom anda adalah 55 - 59")
+elif n=='D':
+ print ("Rentang nilai PrakTekKom anda adalah 45 - 54")
+elif n=='E':
+ print ("Rentang nilai PrakTekKom anda adalah 0 - 44")
+else :
+ print ("Inputan anda salah atau nilai huruf tidak ada pada Standar Nilai")
